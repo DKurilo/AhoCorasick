f681a3af8870c7dcfe965a69e44e6284468c337b
Dima Kurilo <dkurilo@gmail.com>
2020-02-14 15:35:34 -0500

    preparation to make it possible to use wildcards and something like this

--- Text/AhoCorasick.hs
+++ Text/AhoCorasick.hs
@@ -1,4 +1,7 @@
-{-# LANGUAGE RankNTypes, ScopedTypeVariables, ExistentialQuantification #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE RankNTypes                #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
 -- Module:      Text.Hastache
 -- Copyright:   Sergey S Lymar (c) 2012
 -- License:     BSD3
@@ -67,75 +70,93 @@ module Text.AhoCorasick (
     , resetStateMachine
     ) where
 
-import Control.Monad.State.Lazy (execStateT, get, put)
-import Control.Monad.ST.Strict (ST, runST)
-import Control.Monad.Trans (lift)
-import Data.Array.IArray (Array, array, (!))
-import Data.Hashable (Hashable)
-import Data.Maybe (fromJust)
-import Data.STRef (STRef, newSTRef, readSTRef, writeSTRef, modifySTRef)
-import qualified Data.HashMap.Strict as M
+import           Control.Monad.ST.Strict         (ST, runST)
+import           Control.Monad.State.Lazy        (execStateT, get, put)
+import           Control.Monad.Trans             (lift)
+import           Data.Array.IArray               (Array, array, (!))
+import           Data.Hashable                   (Hashable (..))
+import qualified Data.HashMap.Strict             as M
+import           Data.Maybe                      (fromJust)
+import           Data.STRef                      (STRef, modifySTRef, newSTRef,
+                                                  readSTRef, writeSTRef)
+import           Text.AhoCorasick.Internal.Deque (DQ, dqLength, mkDQ, popFront,
+                                                  pushBack)
 
-import Text.AhoCorasick.Internal.Deque (mkDQ, pushBack, popFront, dqLength, DQ)
+data ExtKeySymb keySymb = Symb keySymb | ExtTest keySymb (keySymb -> Bool)
 
-data (Eq keySymb, Hashable keySymb) => TNode keySymb s = 
+getKeySymb :: ExtKeySymb keySymb -> keySymb
+getKeySymb (Symb keySymb)      = keySymb
+getKeySymb (ExtTest keySymb _) = keySymb
+
+instance (Eq keySymb) => Eq (ExtKeySymb keySymb) where
+    et1 == et2 = getKeySymb et1 == getKeySymb et2
+
+instance (Hashable keySymb) => Hashable (ExtKeySymb keySymb) where
+    hashWithSalt salt (Symb keySymb)      = hashWithSalt salt keySymb
+    hashWithSalt salt (ExtTest keySymb _) = hashWithSalt salt keySymb
+
+instance (Show keySymb) => Show (ExtKeySymb keySymb) where
+    show (Symb keySymb)      = "Symb" ++ show keySymb
+    show (ExtTest keySymb _) = "ExtTest" ++ show keySymb
+
+data TNode keySymb s =
     TNode {
-          tnId          :: Int
-        , tnLinks       :: M.HashMap keySymb (STRef s (TNode keySymb s))
-        , tnFail        :: Maybe (STRef s (TNode keySymb s))
-        , tnValuesIds   :: [Int]
+          tnId        :: Int
+        , tnLinks     :: M.HashMap keySymb (STRef s (TNode keySymb s))
+        , tnFail      :: Maybe (STRef s (TNode keySymb s))
+        , tnValuesIds :: [Int]
     }
 
 type KeyLength = Int
 
-data (Eq keySymb, Hashable keySymb) => TTree keySymb val s = 
+data TTree keySymb val s =
     TTree {
-          ttRoot        :: STRef s (TNode keySymb s)
-        , ttLastId      :: STRef s Int
-        , ttValues      :: DQ (KeyLength, val) s
+          ttRoot   :: STRef s (TNode keySymb s)
+        , ttLastId :: STRef s Int
+        , ttValues :: DQ (KeyLength, val) s
     }
 
 type NodeIndex = Int
 
-data (Eq keySymb, Hashable keySymb) => SMElem keySymb = 
+data SMElem keySymb =
     SMElem {
-          smeLinks      :: M.HashMap keySymb NodeIndex
-        , smeFail       :: NodeIndex
-        , smeValuesIds  :: [Int]
-    }
-    
-data (Eq keySymb, Hashable keySymb) => StateMachine keySymb val = 
-    StateMachine {
-          smStates      :: Array NodeIndex (SMElem keySymb)
-        , smValues      :: Array Int (KeyLength, val)
-        , smState       :: Int
+          smeLinks     :: M.HashMap keySymb NodeIndex
+        , smeFail      :: NodeIndex
+        , smeValuesIds :: [Int]
     }
 
-data (Eq keySymb, Hashable keySymb) => SMStepRes keySymb val = 
+data StateMachine keySymb val =
+    StateMachine {
+          smStates :: Array NodeIndex (SMElem keySymb)
+        , smValues :: Array Int (KeyLength, val)
+        , smState  :: Int
+    }
+
+data SMStepRes keySymb val =
     SMStepRes {
-          smsrMatch     :: [(KeyLength, val)]
-        , smsrNextSM    :: StateMachine keySymb val
+          smsrMatch  :: [(KeyLength, val)]
+        , smsrNextSM :: StateMachine keySymb val
     }
 
 data Position val =
     Position {
-          pIndex        :: Int
-        , pLength       :: Int
-        , pVal          :: val
+          pIndex  :: Int
+        , pLength :: Int
+        , pVal    :: val
     }
 
-instance (Eq keySymb, Hashable keySymb, Show keySymb) => 
+instance (Eq keySymb, Hashable keySymb, Show keySymb) =>
                                                     Show (SMElem keySymb) where
     show (SMElem l f v) = concat ["SMElem {smeLinks = ", show l,
         ", smeFail = ", show f,", smeValuesIds = ", show v, "}"]
 
-instance (Eq keySymb, Hashable keySymb, Show keySymb, Show val) => 
+instance (Eq keySymb, Hashable keySymb, Show keySymb, Show val) =>
                                         Show (StateMachine keySymb val) where
     show (StateMachine st vals state) = concat [
         "StateMachine {smStates = ", show st,
         ", smValues = ", show vals, ", smState = ", show state,"}"]
 
-instance (Eq keySymb, Hashable keySymb, Show keySymb, Show val) => 
+instance (Eq keySymb, Hashable keySymb, Show keySymb, Show val) =>
                                             Show (SMStepRes keySymb val) where
     show (SMStepRes f n) = concat [
         "StateMachineStepRes {smsrFound = ", show f,
@@ -157,10 +178,9 @@ initNewTTree :: (Eq keySymb, Hashable keySymb) => ST s (TTree keySymb a s)
 initNewTTree = do
     root <- newSTRef $ TNode rootNodeId M.empty Nothing []
     lid <- newSTRef rootNodeId
-    kw <- mkDQ
-    return $ TTree root lid kw
-    
-mkNewTNode :: (Eq keySymb, Hashable keySymb) => 
+    TTree root lid <$> mkDQ
+
+mkNewTNode :: (Eq keySymb, Hashable keySymb) =>
     TTree keySymb a s -> ST s (TNode keySymb s)
 mkNewTNode tree = do
     modifySTRef lid (+1)
@@ -169,7 +189,7 @@ mkNewTNode tree = do
     where
     lid = ttLastId tree
 
-addKeyVal :: forall val s keySymb. (Eq keySymb, Hashable keySymb) => 
+addKeyVal :: forall val s keySymb. (Eq keySymb, Hashable keySymb) =>
     TTree keySymb val s -> [keySymb] -> val -> ST s ()
 addKeyVal tree key val = addSymb (ttRoot tree) key
     where
@@ -210,10 +230,9 @@ findFailures tree = do
             pushBack dq link
             fRef <- findParentFail link (tnFail node) symb
             f <- readSTRef fRef
-            modifySTRef link (\n -> n {tnFail = Just fRef, 
-                tnValuesIds = (tnValuesIds n) ++ (tnValuesIds f)})
+            modifySTRef link (\n -> n {tnFail = Just fRef,
+                tnValuesIds = tnValuesIds n ++ tnValuesIds f})
             ) $ tnLinks node ~> M.toList
-        return ()
     findParentFail link (Just cfRef) symb = do
         cf <- readSTRef cfRef
         case (M.lookup symb (tnLinks cf), cfRef == root) of
@@ -223,19 +242,19 @@ findFailures tree = do
             (Nothing, True) -> return root
             _ -> findParentFail link (tnFail cf) symb
 
-convertToStateMachine :: forall val s keySymb. (Eq keySymb, Hashable keySymb) => 
-    TTree keySymb val s -> 
+convertToStateMachine :: forall val s keySymb. (Eq keySymb, Hashable keySymb) =>
+    TTree keySymb val s ->
     ST s (StateMachine keySymb val)
 convertToStateMachine tree = do
     size <- readSTRef $ ttLastId tree
     nds <- execStateT (convertNode $ ttRoot tree) []
-    
+
     vlsSize <- dqLength $ ttValues tree
     vls <- mapM (\i -> do
         k <- popFront (ttValues tree)
         return (i,fromJust k)
         ) [0..(vlsSize-1)]
-    
+
     StateMachine (array (0, size) nds) (array (0, vlsSize-1) vls) rootNodeId
         ~> return
     where
@@ -243,7 +262,7 @@ convertToStateMachine tree = do
         (n,l,fail) <- lift $ do
             n <- readSTRef node
             l <- tnLinks n ~> convertLinks
-            fail <- tnFail n ~> fromJust ~> readSTRef >>= return . tnId
+            fail <- tnId <$> (tnFail n ~> fromJust ~> readSTRef)
             return (n,l,fail)
         v <- get
         put $ (tnId n, SMElem l fail (tnValuesIds n)) : v
@@ -254,42 +273,42 @@ convertToStateMachine tree = do
     convertLinks lnksMap = do
         nl <- mapM (\(symb, link) -> do
             l <- readSTRef link
-            return $ (symb, tnId l)
+            return (symb, tnId l)
             ) $ M.toList lnksMap
         return $ M.fromList nl
 
-resetStateMachine :: (Eq keySymb, Hashable keySymb) => 
+resetStateMachine :: (Eq keySymb, Hashable keySymb) =>
     StateMachine keySymb val -> StateMachine keySymb val
 resetStateMachine m = m { smState = rootNodeId }
 
-stateMachineStep :: (Eq keySymb, Hashable keySymb) => 
+stateMachineStep :: (Eq keySymb, Hashable keySymb) =>
     StateMachine keySymb val -> keySymb -> SMStepRes keySymb val
 stateMachineStep sm symb =
     case (M.lookup symb links, currentState == rootNodeId) of
         (Just nextState, _) -> SMStepRes
-            ((smStates sm) ! nextState ~> smeValuesIds ~> convertToVals)
+            (smStates sm ! nextState ~> smeValuesIds ~> convertToVals)
             (sm { smState = nextState })
         (Nothing, True) -> SMStepRes [] sm
-        (Nothing, False) -> stateMachineStep 
-            (sm { smState = smeFail currentNode}) symb            
+        (Nothing, False) -> stateMachineStep
+            (sm { smState = smeFail currentNode}) symb
     where
     currentState = smState sm
-    currentNode = (smStates sm) ! currentState
+    currentNode = smStates sm ! currentState
     links = smeLinks currentNode
-    convertToVals idx = map (\i -> smValues sm ! i) idx
+    convertToVals = map (\i -> smValues sm ! i)
 
-findAll :: (Eq keySymb, Hashable keySymb) => 
+findAll :: (Eq keySymb, Hashable keySymb) =>
     StateMachine keySymb val -> [keySymb] -> [Position val]
-findAll sm str = 
+findAll sm str =
     step (resetStateMachine sm) (zip [0..] str) ~> concat
     where
     step _ [] = []
     step csm ((idx,symb):next) = case stateMachineStep csm symb of
         SMStepRes [] newsm -> step newsm next
-        SMStepRes r newsm -> (map (cnvToPos idx) r) : (step newsm next)      
+        SMStepRes r newsm  -> map (cnvToPos idx) r : step newsm next
     cnvToPos idx (keyLength, val) = Position (idx - keyLength + 1) keyLength val
 
-makeSimpleStateMachine :: (Eq keySymb, Hashable keySymb) => 
+makeSimpleStateMachine :: (Eq keySymb, Hashable keySymb) =>
     [[keySymb]] -> StateMachine keySymb [keySymb]
 makeSimpleStateMachine keys = runST $ do
     tree <- initNewTTree
@@ -297,11 +316,10 @@ makeSimpleStateMachine keys = runST $ do
     findFailures tree
     convertToStateMachine tree
 
-makeStateMachine :: (Eq keySymb, Hashable keySymb) => 
+makeStateMachine :: (Eq keySymb, Hashable keySymb) =>
     [([keySymb], val)] -> StateMachine keySymb val
 makeStateMachine kv = runST $ do
     tree <- initNewTTree
-    mapM_ (\(s,v) -> addKeyVal tree s v) kv
+    mapM_ (uncurry (addKeyVal tree)) kv
     findFailures tree
     convertToStateMachine tree
-
